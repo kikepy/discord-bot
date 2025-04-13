3855a0ce6be629136f012dc87877d30130e8e3d4
kikepy <luise.cohalla@gmail.com>
2025-04-12 21:10:59 -0400

    Initial commit

---
+++ commands/utility_commands.py
@@ -0,0 +1,16 @@
+﻿# File: commands/utility_commands.py
+from discord.ext import commands
+import random
+
+class UtilityCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+    @commands.command()
+    async def coinflip(self, ctx):
+        result = random.choice(['Heads', 'Tails'])
+        await ctx.send(f'You flipped: {result}')
---
+++ config/json_reader.py
@@ -0,0 +1,15 @@
+﻿import json
+
+class TokenReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+
+    def get_token(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                data = json.load(file)
+                return data.get("token")
+        except (FileNotFoundError, json.JSONDecoderError) as e:
+            print(f"Error reading token from file: {e}")
+            return None
